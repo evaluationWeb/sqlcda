5669f3b6a718f68b11966b1a731e7b6085191b1f
mithridatem <mathieumithridate@adrar-formation.com>
2025-02-26 21:09:19 +0100

    test : UserRepository::saveWithRoles

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import com.adrar.sqlcda.model.Roles;
 import com.adrar.sqlcda.model.User;
 import com.adrar.sqlcda.repository.UserRepository;
 
 public class Main {
     public static void main(String[] args) {
+        //ajout d'un User avec un Roles
+        Roles roles = new Roles("User");
+        User user = new User();
+        user.setFirstname("Pole-numérique");
+        user.setLastname("Adrar");
+        user.setEmail("pole-numerique@adrar-numerique.com");
+        user.setPassword("12345");
+        user.setRoles(roles);
+
+        //Ajout en BDD
+        UserRepository.saveWithRoles(user);
 
     }
 }
